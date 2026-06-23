15c27acfe8b7959205594ed581eb886ab22afff6
Phuc Tran <tranchiphuc@gmail.com>
2026-06-23 17:22:20 +0700

    add users.py

--- ToDoAppV3/routers/users.py
+++ ToDoAppV3/routers/users.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 from starlette import status
 from pydantic import BaseModel
+from pwdlib import PasswordHash
 from ..models import Users
-
+from ..dependencies import db_dependency
 
 router = APIRouter(prefix='/users', tags=['users'])
+pwd_context = PasswordHash.recommended()
+
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -14,9 +17,8 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
-
-@router.post("/")
-async def create_user(create_user_request: CreateUserRequest):
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email = create_user_request.email,
         username = create_user_request.username,
@@ -24,12 +26,15 @@ async def create_user(create_user_request: CreateUserRequest):
         last_name = create_user_request.last_name,
         role = create_user_request.role,
         is_active = True,
-        hashed_password = create_user_request.password
+        hashed_password = pwd_context.hash(create_user_request.password)
     )
+    db.add(create_user_model)
+    db.commit()
     return create_user_model
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
-async def get_user():
-    return {'user': 'Test user'}
+async def get_user(db: db_dependency):
+    users = db.query(Users).all()
+    return users
 
